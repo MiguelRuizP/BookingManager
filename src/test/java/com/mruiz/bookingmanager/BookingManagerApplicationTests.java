46c1436f7a20385e141465c00b880b2d7d88281b
MiguelRuizP <miguelruizpryshlyak.sanjose@alumnado.fundacionloyola.net>
2023-02-08 12:19:46 +0100

    Commit inicial

---
+++ src/test/java/com/mruiz/bookingmanager/BookingManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mruiz.bookingmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
