bf0aad7f3ea11a79a615a476b40292e3e5d0e33d
MiguelRuizP <miguelruizpryshlyak.sanjose@alumnado.fundacionloyola.net>
2023-02-16 12:27:14 +0100

    Añadido método para conseguir usuario a partir del token

--- src/main/java/com/mruiz/bookingmanager/service/TokenService.java
+++ src/main/java/com/mruiz/bookingmanager/service/TokenService.java
@@ -7,7 +7,9 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
@@ -16,11 +18,10 @@ import org.springframework.stereotype.Service;
 public class TokenService {
 	
 	@Autowired
-	private final JwtEncoder encoder;
-
-	public TokenService(JwtEncoder encoder) {
-		this.encoder = encoder;
-	}
+	private JwtEncoder encoder;
+	
+	@Autowired
+	private JwtDecoder decoder;
 
 	public String generateToken(Authentication authentication) {
 		Instant now = Instant.now();
@@ -30,5 +31,14 @@ public class TokenService {
 				.expiresAt(now.plus(1, ChronoUnit.HOURS)).subject(authentication.getName()).claim("scope", scope)
 				.build();
 		return this.encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+	}
+	
+	public String getUserFromToken(String token) {
+		return decoder.decode(token).getSubject();
+	}
+	
+	public String getUserFromAuthorities(Authentication auth) {
+		String token = ((Jwt)auth.getCredentials()).getTokenValue();
+		return getUserFromToken(token);
 	}
 }
