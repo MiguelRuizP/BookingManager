f852b09936a527b2397ea3d59376517a7e77ccec
MiguelRuizP <miguelruizpryshlyak.sanjose@alumnado.fundacionloyola.net>
2023-02-22 09:04:55 +0100

    Cambio en el cron del job

--- src/main/java/com/mruiz/bookingmanager/Scheduler.java
+++ src/main/java/com/mruiz/bookingmanager/Scheduler.java
@@ -2,6 +2,8 @@ package com.mruiz.bookingmanager;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -13,6 +15,8 @@ import com.mruiz.bookingmanager.service.impl.EmailTemplateService;
 
 @Component
 public class Scheduler {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired
 	private BookingRepository bookingRepository;
@@ -21,13 +25,17 @@ public class Scheduler {
 	private EmailTemplateService emailTemplateService;
 
 	@Transactional
-	@Scheduled(cron = "5 * * * * *")
+	@Scheduled(cron = "0 */5 * * * *")
 	public void emailAlertJob() {
+		logger.info("Ejecutando Job de alertas de correo");
+		
 		List<Booking> unnotified = bookingRepository.findUnnotified();
 		
 		unnotified.forEach(booking -> {
 			emailTemplateService.alertBooking(booking);
 			bookingRepository.checkNotified(booking.getId());
 		});
+		
+		logger.info("Terminado Job de alertas de correo");
 	}
 }
--- src/main/java/com/mruiz/bookingmanager/controller/TestController.java
+++ src/main/java/com/mruiz/bookingmanager/controller/TestController.java
@@ -1,22 +1,16 @@
 package com.mruiz.bookingmanager.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mruiz.bookingmanager.payload.SimpleMessageDto;
-import com.mruiz.bookingmanager.repository.BookingRepository;
 
 @RestController
 public class TestController {
 	
-	@Autowired
-	private BookingRepository bookingRepository;
-	
 	@PostMapping("/test")
 	public ResponseEntity<?> test(){
-		int id = bookingRepository.findUnnotified().get(0).getId();
-		return ResponseEntity.ok(new SimpleMessageDto("test " + id));
+		return ResponseEntity.ok(new SimpleMessageDto("test"));
 	}
 }
